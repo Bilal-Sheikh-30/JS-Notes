bffc3a63ed494c3366dd03af9a23bf710d90f885
Bilal Ahmed Sheikh <144335865+Bilal-Sheikh-30@users.noreply.github.com>
2023-09-16 16:39:24 +0000

    added conditionals file

---
+++ conditionals.js
@@ -0,0 +1,38 @@
+//          IF STATEMENT
+
+// if (true) {
+//     console.log("code will be executed");
+// }
+
+//      IF, ELSE AND MULTIPLE IFs
+// amount = 500
+// if (amount > 1000) {
+//     console.log("greater than 1000");
+// } else if (amount > 750) {
+//     console.log("greater than 750");
+// }else if (amount > 500) {
+//     console.log("greater than 500");
+// }else{
+//     console.log("less than 500");
+// }
+
+//      single line if statement
+// if (true) console.log("single line if");    // its scope is before ;
+
+//  and ( executed only when all conditions are true) &&
+let condition1 = true
+let condition2 = true
+let condition3 = false
+// if (condition1 && condition2) {
+//     console.log("executed");
+// } else{
+//     console.log("not executed");
+// }
+
+//  or ( executed  when any condition is true) ||
+// if (condition1 || condition3) {
+//     console.log("executed");
+// } else {
+//     console.log("else executed");
+// }
+
