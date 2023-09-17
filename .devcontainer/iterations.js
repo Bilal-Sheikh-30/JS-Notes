2676286c4565a20873ddcf3fe2bd782ffaeb920e
Bilal Ahmed Sheikh <144335865+Bilal-Sheikh-30@users.noreply.github.com>
2023-09-17 09:55:46 +0000

    added: for, while and do while loops

---
+++ .devcontainer/iterations.js
@@ -0,0 +1,39 @@
+//      FOR LOOP
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);
+    
+// }
+
+//      BREAK
+// for (let num = 1; num < 10; num++) {
+//     if (num == 6) {
+//         console.log("detected 6");
+//         break
+//     }
+//     console.log(num);
+// }
+
+//     Continue
+// for (let num = 1; num < 10; num++) {
+//     if (num == 6) {
+//         console.log("detected 6");
+//         continue
+//     }
+//     console.log(num);
+// }
+
+//      WHILE LOOP
+
+// let index = 1
+// while (index < 6) {
+//     console.log(index);
+//     index ++
+// }
+
+//  DO WHILE    => first performs the operation and then check the condition
+let score = 1
+// let score = 6
+// do {
+//     console.log(`score is: ${score}`);
+//     score ++
+// } while (score <= 5);
